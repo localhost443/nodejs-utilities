f7a1a96135bd1642cee11d43219486ff2b6c11c9
Sakhawat Hossen <sakhawath2009@gmail.com>
2023-09-20 10:13:25 +0600

    added required module and comments on password encryptor

--- password_encrypt_check_sha256_with_salt.js
+++ password_encrypt_check_sha256_with_salt.js
@@ -1,10 +1,14 @@
+const crypto = require("crypto");
+
 const encryptPassword = (password) => {
+  //Creating a random salt of 64 byte to use with each account
+  //Each account will have its own salt
   const salt = crypto.randomBytes(64).toString("hex");
   const hashedPassword = crypto
     .createHash("sha512")
     .update(password + salt)
     .digest("hex");
-
+  //Returning the salt along with encrypted password, you need to save both salt and the password
   return {
     salt: salt,
     encryptedPassword: hashedPassword,
@@ -12,12 +16,13 @@ const encryptPassword = (password) => {
 };
 
 const checkPassword = (plainTextPassword, encryptedPassword, salt) => {
+  //Encrypting the password again with the salt
   const hashedEnteredPassword = crypto
     .createHash("sha512")
     .update(plainTextPassword + salt)
     .digest("hex");
-
+  //comparing converted password with stored password, and returning
   return hashedEnteredPassword === encryptedPassword;
 };
-
+//Exporting the module
 module.exports = { encryptPassword, checkPassword };
